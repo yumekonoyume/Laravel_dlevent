ab1b4d1af8f2cf05dbb4414dbf7bf6c28c1c569a
yumekonoyume <dotslab.yukko@gmail.com>
2024-12-23 16:02:51 +0900

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
+// ↑↑ CommonJS形式からESモジュール形式からへ変更
+// 修正前：
+// export default {
+//     plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//     },
+//   };
